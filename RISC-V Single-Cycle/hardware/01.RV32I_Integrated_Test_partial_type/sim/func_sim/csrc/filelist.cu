0de91b55bcfe7a2015ba023880fbec221cd8a37c
JuNyeong <korea.junyeong@gmail.com>
2025-12-02 01:44:04 +0900

    #Optimization

--- RISC-V Single-Cycle/hardware/01.RV32I_Integrated_Test_partial_type/sim/func_sim/csrc/filelist.cu
+++ RISC-V Single-Cycle/hardware/01.RV32I_Integrated_Test_partial_type/sim/func_sim/csrc/filelist.cu
@@ -4,16 +4,16 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _495106_archive_1.so
-_495106_archive_1.so : archive.4/_495106_archive_1.a
+ARCHIVE_OBJS += _638518_archive_1.so
+_638518_archive_1.so : archive.5/_638518_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_495106_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_638518_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_495106_archive_1.so $@
+	@ln -sf .//../simv.daidir//_638518_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.4/_prev_archive_1.a
+_prev_archive_1.so : archive.5/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
--- RISC-V Single-Cycle/hardware/03.RV32I_Integrated_Test_update/sim/func_sim/run.f
+++ RISC-V Single-Cycle/hardware/03.RV32I_Integrated_Test_update/sim/func_sim/run.f
@@ -1,38 +1,33 @@
 ./sim_define.v
 
-// ../../src/rtl/refCPU/rev00_protect/ASYNC_RAM_DP_WBE.v
-// ../../src/rtl/refCPU/rev00_protect/reg_file_async.v
-// ../../src/rtl/refCPU/rev00_protect/rv32i_single_cycle_cpu_protected.vp
-// ../../src/rtl/refCPU/rev00_protect/RV32I_System.v
-
 //memory
-../../../source/single_cycle/myCPU/rev00/model/ASYNC_RAM_DP_WBE.v
+../../../source/rev00/model/ASYNC_RAM_DP_WBE.v
 
 
 // Register File
-../../../source/single_cycle/myCPU/rev00/src/rtl/reg_file_async.v
+../../../source/rev00/src/rtl/reg_file_async.v
 
 
 //block
-../../../source/single_cycle/myCPU/rev00/src/rtl/building_blocks/adder.sv
-../../../source/single_cycle/myCPU/rev00/src/rtl/building_blocks/extend.sv
-../../../source/single_cycle/myCPU/rev00/src/rtl/building_blocks/flopenr.sv
-../../../source/single_cycle/myCPU/rev00/src/rtl/building_blocks/flopr.sv
-../../../source/single_cycle/myCPU/rev00/src/rtl/building_blocks/be_logic.sv
-../../../source/single_cycle/myCPU/rev00/src/rtl/building_blocks/mux2.sv
-../../../source/single_cycle/myCPU/rev00/src/rtl/building_blocks/mux3.sv
+../../../source/rev00/src/rtl/building_blocks/adder.sv
+../../../source/rev00/src/rtl/building_blocks/extend.sv
+../../../source/rev00/src/rtl/building_blocks/flopenr.sv
+../../../source/rev00/src/rtl/building_blocks/flopr.sv
+../../../source/rev00/src/rtl/building_blocks/be_logic.sv
+../../../source/rev00/src/rtl/building_blocks/mux2.sv
+../../../source/rev00/src/rtl/building_blocks/mux3.sv
 
-../../../source/single_cycle/myCPU/rev00/src/rtl/alu.sv
-../../../source/single_cycle/myCPU/rev00/src/rtl/aludec.sv
-../../../source/single_cycle/myCPU/rev00/src/rtl/maindec.sv
-../../../source/single_cycle/myCPU/rev00/src/rtl/branch_logic.sv
+../../../source/rev00/src/rtl/alu.sv
+../../../source/rev00/src/rtl/aludec.sv
+../../../source/rev00/src/rtl/maindec.sv
+../../../source/rev00/src/rtl/branch_logic.sv
 
-../../../source/single_cycle/myCPU/rev00/src/rtl/controller.sv
-../../../source/single_cycle/myCPU/rev00/src/rtl/datapath.sv
+../../../source/rev00/src/rtl/controller.sv
+../../../source/rev00/src/rtl/datapath.sv
 
-../../../source/single_cycle/myCPU/rev00/src/rtl/riscvsingle.sv
+../../../source/rev00/src/rtl/riscvsingle.sv
 
-../../../source/single_cycle/myCPU/rev00/src/rtl/SMU_RV32I_System.v
+../../../source/rev00/src/rtl/SMU_RV32I_System.v
 
 
 //testbench
